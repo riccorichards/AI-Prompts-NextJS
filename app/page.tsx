0cb0c15706ce3a022bf8eabeb725903f6c2b3823
Riccorichards <richard.tropimovi.1@iliauni.edu.ge>
2024-02-28 03:01:29 +0400

    latest update

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Feed from "@/components/Feed";
 
-const Home = () => {
+const Home = async () => {
   return (
     <section className="w-full flex-center flex-col">
       <h1 className="head_text text-center">
